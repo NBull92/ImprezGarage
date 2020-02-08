b6ba9245c8a4079454b2129a1f900e2bb188b2fd
Nbull92 <nickbull@nickbull-computing.biz>
2020-02-08 17:23:12 +0000

    Update PartsReplacementRecords.cs
    Ready for Report Repairs feature

--- Src/Business/ImprezGarage.Infrastructure/Model/PartsReplacementRecords.cs
+++ Src/Business/ImprezGarage.Infrastructure/Model/PartsReplacementRecords.cs
@@ -1,7 +1,14 @@
-﻿namespace ImprezGarage.Infrastructure.Model
+﻿using System;
+
+namespace ImprezGarage.Infrastructure.Model
 {
     public class PartsReplacementRecords
     {
-        
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string PartReplaced { get; set; }
+        public string ReplacedWith { get; set; }
+        public decimal Price { get; set; }
     }
 }
