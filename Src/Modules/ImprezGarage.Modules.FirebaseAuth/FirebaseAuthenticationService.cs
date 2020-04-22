4e791ff80f0547980f6154d92e970584fc3bb840
Nbull92 <nickbull@nickbull-computing.biz>
2020-04-22 21:08:21 +0100

    Seperated into own cs file

--- Src/Modules/ImprezGarage.Modules.FirebaseAuth/FirebaseAuthenticationService.cs
+++ Src/Modules/ImprezGarage.Modules.FirebaseAuth/FirebaseAuthenticationService.cs
@@ -8,11 +8,9 @@ namespace ImprezGarage.Modules.FirebaseAuth
     using Microsoft.Practices.ServiceLocation;
     using Prism.Regions;
     using System;
-    using System.Runtime.InteropServices;
     using System.Security;
     using System.Threading.Tasks;
     using Views;
-
 
     internal class FirebaseAuthenticationService : IAuthenticationService
     {
@@ -69,66 +67,6 @@ namespace ImprezGarage.Modules.FirebaseAuth
                 throw new NullReferenceException("No user is currently signed in.");
 
             return _currentUser;
-        }
-    }
-
-    public static class SecureHelper
-    {
-        public static string SecureStringToString(SecureString value)
-        {
-            var valuePtr = IntPtr.Zero;
-            try
-            {
-                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
-                return Marshal.PtrToStringUni(valuePtr);
-            }
-            finally
-            {
-                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
-            }
-        }
-
-        public static bool SecureStringEqual(SecureString secureString1, SecureString secureString2)
-        {
-            if (secureString1 == null)
-            {
-                throw new ArgumentNullException("s1");
-            }
-            if (secureString2 == null)
-            {
-                throw new ArgumentNullException("s2");
-            }
-
-            if (secureString1.Length != secureString2.Length)
-            {
-                return false;
-            }
-
-            IntPtr ss_bstr1_ptr = IntPtr.Zero;
-            IntPtr ss_bstr2_ptr = IntPtr.Zero;
-
-            try
-            {
-                ss_bstr1_ptr = Marshal.SecureStringToBSTR(secureString1);
-                ss_bstr2_ptr = Marshal.SecureStringToBSTR(secureString2);
-
-                String str1 = Marshal.PtrToStringBSTR(ss_bstr1_ptr);
-                String str2 = Marshal.PtrToStringBSTR(ss_bstr2_ptr);
-
-                return str1.Equals(str2);
-            }
-            finally
-            {
-                if (ss_bstr1_ptr != IntPtr.Zero)
-                {
-                    Marshal.ZeroFreeBSTR(ss_bstr1_ptr);
-                }
-
-                if (ss_bstr2_ptr != IntPtr.Zero)
-                {
-                    Marshal.ZeroFreeBSTR(ss_bstr2_ptr);
-                }
-            }
         }
     }
 }
