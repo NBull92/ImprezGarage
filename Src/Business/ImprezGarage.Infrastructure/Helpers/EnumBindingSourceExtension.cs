c1fe3661c22eef2cd182570a4806d88f80c836fb
NBull92 <nickbull@nickbull-computing.biz>
2020-08-17 21:58:38 +0100

    Updating How To Bind to Enums
    Used Brain Lagunas' way of binding to enums.
    It's much cleaner and easier to work with.
    
    https://www.youtube.com/watch?v=Bp5LFXjwtQ0

---
+++ Src/Business/ImprezGarage.Infrastructure/Helpers/EnumBindingSourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace ImprezGarage.Modules.Logger.Helpers
+{
+    /// <summary>
+    /// This class is used to access the values of an enum and allow for us to then bind those values to a UI Element like a ComboBox
+    /// </summary>
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }  
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if(enumType is null || !enumType.IsEnum)
+                throw new Exception("EnumType should be of Type Enum.");
+
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
